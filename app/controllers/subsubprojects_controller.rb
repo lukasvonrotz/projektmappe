a34dae7a327012a560f8374900028fe9fd511fed
Lukas von Rotz <lukas.vonrotz@prola.ch>
2017-09-18 13:50:49 +0200

    project and versions complete

--- app/controllers/subsubprojects_controller.rb
+++ app/controllers/subsubprojects_controller.rb
@@ -8,12 +8,22 @@ class SubsubprojectsController < ApplicationController
   def new
     # build a 'temporary' post which is written to DB later (create-method)
     @subsubproject = Subsubproject.new
+    @subprojectid = params[:subproject_id]
   end
 
   # Control logic when creating a new subsubproject
   # POST /subsubprojects
   def create
     @subsubproject = Subsubproject.new(subsubproject_params)
+    if params[:subsubproject][:master] == "1"
+      @subproject = Subproject.find(params[:subsubproject][:subproject_id])
+      @subproject.subsubprojects.each do |othersubsubproject|
+        if othersubsubproject.master
+          othersubsubproject.master = false
+          othersubsubproject.save
+        end
+      end
+    end
     # write subsubproject to database
     if @subsubproject.save
       redirect_to project_subproject_path(:project_id => @subsubproject.subproject.project.id, :id => @subsubproject.subproject.id), :notice => 'Version erfolgreich erstellt.'
@@ -40,6 +50,14 @@ class SubsubprojectsController < ApplicationController
   # PUT /subsubprojects/:id
   def update
     @subsubproject = Subsubproject.find(params[:id])
+    if params[:subsubproject][:master] == "1"
+      @subsubproject.subproject.subsubprojects.each do |othersubsubproject|
+        if othersubsubproject.master
+          othersubsubproject.master = false
+          othersubsubproject.save
+        end
+      end
+    end
     if @subsubproject.update(subsubproject_params)
       redirect_to project_subproject_path(:project_id => @subsubproject.subproject.project.id,
                                           :id => @subsubproject.subproject.id),
